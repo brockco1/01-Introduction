41563564b26997b9ad244921efa022fc17cb23c4
brockco1 <brockco@iu.edu>
2019-09-10 22:54:55 -0500

    I changed the License, Readme  and answered the six questions for this asssignment

--- main.py
+++ main.py
@@ -5,4 +5,9 @@ import utils
 utils.check_version((3,7))
 utils.clear()
 
-print('Hello, Class!')
+print('Hello, my name is Cortland Brock')
+print('I dont have a favorite game')
+print('I fear that I will need a better understanding of coding inorder to take this class') 
+print('Im exited about being able to learn a vauable skill and to be able work on my own games')
+print('my stockoverflow.com user number is 12048479')
+print('the URL to my github.com proflie is https://github.com/brockco1')
